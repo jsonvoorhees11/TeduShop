c10f08a7daa34d011d45fd9566155185a64d1936
koenigseggone <little.shadowfiend@gmail.com>
2018-04-23 18:03:08 +0700

    Some first models and interfaces

---
+++ TeduShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeduShop.Model.Abstract
+{
+    public interface IAuditable
+    {
+        DateTime? CreatedDate { get; set; }
+        string CreatedBy { get; set; }
+        DateTime? UpdateDate { get; set; }
+        string UpdateBy { get; set; }
+    }
+}
---
+++ TeduShop.Model/Abstract/ISeoable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Abstract
+{
+    public interface ISeoable
+    {
+        string MetaKeyword { get; set; }
+        string MetaDecription { get; set; }
+    }
+}
