a67e996ad4425635e43e3c93d7ae0d3e072fbeb6
koenigseggone <little.shadowfiend@gmail.com>
2018-04-24 00:21:14 +0700

    Stage Entity Classes

--- TeduShop.Model/Models/MenuGroup.cs
+++ TeduShop.Model/Models/MenuGroup.cs
@@ -1,22 +1,21 @@
-﻿using System;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TeduShop.Model.Models
 {
+    [Table("MenuGroups")]
     public class MenuGroup
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int ID { get; set; }
+        public int ID { set; get; }
 
         [Required]
-        public string Name { get; set; }
+        [MaxLength(50)]
+        public string Name { set; get; }
 
-        public virtual IEnumerable<Menu> Menus { get; set; }
+        public virtual IEnumerable<Menu> Menus { set; get; }
     }
 }
--- TeduShop.Model/Models/Product.cs
+++ TeduShop.Model/Models/Product.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Linq;
 using TeduShop.Model.Abstract;
 
@@ -15,38 +10,39 @@ namespace TeduShop.Model.Models
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int ID { get; set; }
+        public int ID { set; get; }
 
         [Required]
-        public string Name { get; set; }
+        [MaxLength(256)]
+        public string Name { set; get; }
 
         [Required]
-        public string Alias { get; set; }
-        
-        public int CategoryID { get; set; }
+        [MaxLength(256)]
+        public string Alias { set; get; }
+
+        [Required]
+        public int CategoryID { set; get; }
+
+        [MaxLength(256)]
+        public string Image { set; get; }
+
+        [Column(TypeName = "xml")]
+        public string MoreImages { set; get; }
+
+        public decimal Price { set; get; }
+
+        public decimal? PromotionPrice { set; get; }
+        public int? Warranty { set; get; }
+
+        [MaxLength(500)]
+        public string Description { set; get; }
+        public string Content { set; get; }
+
+        public bool? HomeFlag { set; get; }
+        public bool? HotFlag { set; get; }
+        public int? ViewCount { set; get; }
 
         [ForeignKey("CategoryID")]
-        public virtual ProductCategory ProductCategory { get; set; }
-
-        public string Image { get; set; }
-
-        public XElement MoreImages { get; set; }
-
-        public decimal Price { get; set; }
-
-        public decimal? Promotion { get; set; }
-
-        public int? Warranty { get; set; }
-
-        public string Decription { get; set; }
-
-        public string Content { get; set; }
-
-
-        public bool? HomeFlag { get; set; }
-
-        public bool? HotFlag { get; set; }
-
-        public int? ViewCount { get; set; }
+        public virtual ProductCategory ProductCategory { set; get; }
     }
 }
