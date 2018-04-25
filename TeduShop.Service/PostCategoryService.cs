7c5f11f7a012aae0f10c2650238da51555e38283
koenigseggone <little.shadowfiend@gmail.com>
2018-04-25 14:43:23 +0700

    Add commit() to postcategory service

--- TeduShop.Service/PostCategoryService.cs
+++ TeduShop.Service/PostCategoryService.cs
@@ -18,6 +18,8 @@ namespace TeduShop.Service
         IEnumerable<PostCategory> GetAllByParentId(int parentID);
 
         PostCategory GetById(int id);
+
+        void SaveChanges();
     }
 
     public class PostCategoryService : IPostCategoryService
@@ -54,6 +56,11 @@ namespace TeduShop.Service
         public PostCategory GetById(int id)
         {
             return _postCategoryRepository.GetSingleById(id);
+        }
+
+        public void SaveChanges()
+        {
+            _unitOfWork.Commit();
         }
 
         public void Update(PostCategory postCategory)
