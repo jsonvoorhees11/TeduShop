ad11b95788701a52cefc4d97e3431bd7632c9d81
koenigseggone <little.shadowfiend@gmail.com>
2018-04-25 14:40:12 +0700

    Stage postservice and postcategoryservice

--- TeduShop.Service/PostService.cs
+++ TeduShop.Service/PostService.cs
@@ -15,6 +15,7 @@ namespace TeduShop.Service
         void Update(Post post);
         void Delete(int id);
         IEnumerable<Post> GetAll();
+        IEnumerable<Post> GetAllByCategoryPaging(int categoryId, int page, int pageSize, out int totalRow); 
         IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow);
         IEnumerable<Post> GetAllByTagPaging(string tag, int page, int pageSize, out int totalRow );
         Post GetById(int id);
@@ -45,10 +46,15 @@ namespace TeduShop.Service
             return _postRepository.GetAll(new string[] { "PostCategory" });
         }
 
+        public IEnumerable<Post> GetAllByCategoryPaging(int categoryId, int page, int pageSize, out int totalRow)
+        {
+            return _postRepository.GetMultiPaging(x => (x.Status == true && x.CategoryID == categoryId), out totalRow, page, pageSize, new string[] { "PostCategory" });
+        }
+
         public IEnumerable<Post> GetAllByTagPaging(string tag, int page, int pageSize, out int totalRow)
         {
             //TODO: Select all posts by tag
-            return _postRepository.GetMultiPaging(x => x.Status,out totalRow, page, pageSize);
+            return _postRepository.GetAllByTag(tag, page, pageSize, out totalRow);
         }
 
         public IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow)
