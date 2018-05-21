6d6d367fdbd9c13bab044f35ae29c09281c8c366
koenigseggone <little.shadowfiend@gmail.com>
2018-05-21 20:19:50 +0700

    scope in AngularJS

---
+++ TeduShop.Web/Scripts/SPA/app.js
@@ -0,0 +1,17 @@
+﻿
+
+var myApp = angular.module('myModule', []);
+myApp.controller("animalController", animalController);
+myApp.controller("dogController", dogController);
+myApp.controller("catController", catController);
+//myController.$inject = ['$scope'];
+
+function animalController($scope) {
+    $scope.message = "animal message";
+}
+function dogController($scope) {
+    $scope.message = "woof woof";
+}
+function catController($scope) {
+    $scope.message = "mew mew";
+}
