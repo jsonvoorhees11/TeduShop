8864614e62b44afd8db36beb1ba0ed990a21f8da
koenigseggone <little.shadowfiend@gmail.com>
2018-05-21 23:55:06 +0700

    Services in AngularJS

--- TeduShop.Web/Scripts/SPA/app.js
+++ TeduShop.Web/Scripts/SPA/app.js
@@ -2,16 +2,36 @@
 
 var myApp = angular.module('myModule', []);
 myApp.controller("animalController", animalController);
+myApp.service("validator", validator);
+
+animalController.$inject = ['$scope', 'validator'];
 myApp.controller("dogController", dogController);
 myApp.controller("catController", catController);
-//myController.$inject = ['$scope'];
 
-function animalController($scope) {
-    $scope.message = "animal message";
+function animalController($scope, validator) {
+    $scope.checkEven = function()
+    {
+        $scope.message = validator.checkNumber($scope.num);
+    }
+    $scope.num = 1;
 }
 function dogController($scope) {
     $scope.message = "woof woof";
 }
 function catController($scope) {
     $scope.message = "mew mew";
+}
+
+function validator($window) {
+    return {
+        checkNumber: checkNumber
+    }
+    function checkNumber(input) {
+        if (input % 2 == 0) {
+            return "This is even";
+        }
+        else {
+            return "This is odd";
+        }
+    } 
 }
